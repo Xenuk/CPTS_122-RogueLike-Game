2707849ef16e81a87e19105f04cf929d82a1dddb
Axel Garvin <axel.garvin@wsu.edu>
2025-11-21 09:27:52 -0800

    Added
    Corrected include directive and formatting issues.

--- Projectile.hpp
+++ Projectile.hpp
@@ -1,13 +1,21 @@
+/*
+File Name: Projectile.hpp
+Created: 11/21/2025
+Purpose: Projectile content file
+*/
+
 #pragma once
-#include "header.hpp"
+#include "Header.hpp"
 
 
-class Projectile : public sf::Sprite {
+class Projectile : public sf::Sprite 
+{
 
 public:
 	Projectile(const sf::Texture& texture, int nDamage, sf::Vector2f nDirectionAndSpeed, double nLifeTime); // nDirectionAndSpeed later will be used from mouse position calculated somehow or smth.
 	~Projectile();
 	sf::Vector2f directionAndSpeed;
+
 private:
 	int damage;
 	double lifeTime;
